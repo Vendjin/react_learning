150b7f7d375a0ec1dd083a4c09747c8b5edaf74f
Andrey_Komarovskiy <andruxa325@gmail.com>
2023-01-31 09:29:35 +0300

    доделал random character

--- marvel-project/src/components/randomChar/RandomChar.js
+++ marvel-project/src/components/randomChar/RandomChar.js
@@ -16,12 +16,19 @@ const RandomChar = () => {
                     </div>
 
                     <div className='randomChar__btns'>
-                        <button className=" button button__main">Тектс</button>
-                        <button className=" button button__secondary">Текс2</button>
+                        <button className=" button button__main">HOMEPAGE</button>
+                        <button className=" button button__secondary">WIKI</button>
                     </div>
                 </div>
             </div>
             <div className="randomChar__static">
+                <div className="randomChar__title">Random character for today!<br/>
+                    Do you want to get to know him better?
+                </div>
+                <div className="randomChar__title">Or choose another one</div>
+                <button className=" button button__main">TRY IT</button>
+                <img src={hammer} alt="Random Character" className='randomChar__decoration'/>
+
 
             </div>
 

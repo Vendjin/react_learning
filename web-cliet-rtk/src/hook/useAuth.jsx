ab1a3f6dcb7fda1f57de324681d9df9a5f2cfb2c
Andrey_Komarovskiy <adnruxa325@gmail.com>
2023-10-06 12:13:16 +0300

    удалил не нужный репозиторий

--- web-cliet-rtk/src/hook/useAuth.jsx
+++
@@ -1,7 +0,0 @@
-import {useSelector} from "react-redux";
-
-export const useAuth = () => {
-    return !!sessionStorage.getItem('token')
-    // const {isLogged} = useSelector(state => state.auth);
-    // return isLogged
-}
